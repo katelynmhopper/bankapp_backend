d087d3ed6d59fffe9ee9656da4a6ad529080c142
Katelyn Hopper <katelynmhopper@gmail.com>
2022-10-06 00:56:28 -0600

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectionURL = 'mongodb://127.0.0.1:27017'; 
 const databaseName = 'bank-customers';
 
-mongoose.connect(mongoose.connect('mongodb+srv://katelynmhopper:<Adalyn102912>@cluster0.t2zufr3.mongodb.net/?retryWrites=true&w=majority'), {
+mongoose.connect('mongodb+srv://katelynmhopper:<Adalyn102912>@cluster0.t2zufr3.mongodb.net/?retryWrites=true&w=majority'),
+ {
     useNewUrlParser: true, 
 };
 
